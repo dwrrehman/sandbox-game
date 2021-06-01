588bfa349511bd154ea6774155828cf046828000
dwrrehman <dwrrehman@gmail.com>
2021-06-01 16:57:19 -0700

    push the new testing world that im going to be working in.

--- client.c
+++ client.c
@@ -33,7 +33,6 @@ typedef int32_t i32;
 typedef int64_t i64;
 
 
-
 static const u8 colors[] = {
 	0,0,0,   	// 0
 	255,255,255,    // 1
@@ -169,7 +168,6 @@ int main(const int argc, const char** argv) {
 	const u32 max_block_count = 10000000;
 	u16* screen = malloc(max_block_count * 2);
 
-
 	char player_name[30] = {0};
 	strncpy(player_name, argv[3], sizeof player_name);
 
@@ -193,7 +191,6 @@ int main(const int argc, const char** argv) {
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	SDL_ShowCursor(0);
-
 	window_changed(window, renderer);
 	send_resize_command(connection);
 	
@@ -202,7 +199,6 @@ int main(const int argc, const char** argv) {
 		
 		command = display;
 		write(connection, &command, 1);
-
 		n = read(connection, &screen_block_count, sizeof(u32)); 
 		check(n);
 		
@@ -212,20 +208,9 @@ int main(const int argc, const char** argv) {
 			// if (n <= 0) continue; // retry this packet. // infinite loop?
 			local_count += 128;
 		}
-		
 		// printf("display: received %d blocks, rendering...\n", screen_block_count);
-
 		int width_radius = (scaled_width - 1) >> 1;
 		int height_radius = (scaled_height - 1) >> 1;
-		int _i = 0,_j = 0,x_off,y_off;
-		for (_i = 0, x_off = -width_radius; _i < scaled_width; _i++, x_off++) {
-			for (_j = 0, y_off = -height_radius; _j < scaled_height; _j++, y_off++) {
-				if (not x_off and not y_off) goto double_break_out;
-			}
-		}
-	
-	double_break_out:
-		
 
 		// clear:
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
@@ -233,7 +218,7 @@ int main(const int argc, const char** argv) {
 
 		// print player:			
 		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-		SDL_RenderDrawPoint(renderer, _i, _j);
+		SDL_RenderDrawPoint(renderer, width_radius, height_radius);
 
 		// print space:
 		SDL_SetRenderDrawColor(renderer, colors[4], colors[5], colors[6], 255);
@@ -296,347 +281,8 @@ int main(const int argc, const char** argv) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ---------------------------------- dead code -------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sendto(udp_connection, &command, 1, 0, (struct sockaddr*)&cliaddr, len);
-// sendto(udp_connection, &ack, 1, 0, (struct sockaddr*)&cliaddr, len);
-
-
-
-
-
-
-//        sendto(udp_connection, buffer, sizeof buffer, 0, (struct sockaddr*) &servaddr, len);
-
-//        ssize_t n = recvfrom(udp_connection, buffer, sizeof buffer, 0, (struct sockaddr*) &servaddr, &len);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//if (argc <= 3) return fprintf(stderr, "usage:\n\t ./u <playername> <ipaddress> <port>\n\n");
-	// TCP_connect_to_server(argv[1], argv[2], atoi(argv[3]));
-
-
-
-// static inline int connect_to_server(const char* address, int port, int* connection) {
-// 	printf("info: connecting to \"%s:%d\"...\n", address, port);
-// 	*connection = 0;
-// 	return 0;
-	
-// }
-
-
-
-
-
-
-
-
-
-
-
-/*
-static inline void UDP_connect_to_server(const char* playername, const char* ip, unsigned int port) {
-    int connection = socket(AF_INET, SOCK_DGRAM, 0);
-    if (connection < 0) { perror("socket"); exit(1); }
-    
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_addr.s_addr = inet_addr(ip);
-    servaddr.sin_port = htons(port);
-    servaddr.sin_family = AF_INET;
-    socklen_t len = sizeof(servaddr);
-    
-    printf("%s is connecting to UDP server...\n", playername);
-        
-    char buffer[1024] = {0};
-
-    while (1) {
-        printf("UDP CLIENT[%s:%d]:> ", ip, port);
-        fgets(buffer, sizeof buffer, stdin);
-        if (!strcmp(buffer, "quit\n")) break;
-        sendto(connection, buffer, sizeof buffer, 0, (struct sockaddr*) &servaddr, len);
-
-        memset(buffer, 0, sizeof buffer);
-        ssize_t n = recvfrom(connection, buffer, sizeof buffer, 0, (struct sockaddr*) &servaddr, &len);
-        if (n == 0) {
-            printf("UDP client:read disconnected.\n");
-            printf("{UDP SERVER DISCONNECTED}\n");
-            break;
-        }
-        printf("UDP server says: %s\n", buffer);
-    }
-    close(connection);
-}
-*/
-
-
-
-
-
-
-
-
-
-// for (size_t i = 0; i < count; i += 2) {
-		// 	array[i + 0] = (uint16_t) (rand() % window_width);
-		// 	array[i + 1] = (uint16_t) (rand() % window_height);
-		// }
-
-
-
-
-
-
-		// } else if (not strcmp(buffer, "chat\n")) {
-
-		// 	u8 command = chat;
-		// 	write(connection, &command, 1);
-		// 	printf("message: ");
-		// 	fgets(buffer, sizeof buffer, stdin);
-		// 	write(connection, buffer, strlen(buffer) + 1);
-		// 	ssize_t n = read(connection, &response, sizeof response);
-		// 	if (n == 0) { printf("{SERVER DISCONNECTED}\n"); break; }
-		// 	else if (n < 0) { read_error(); break; }
-			// if (response != 1) not_acked();
-
-
-
-// we receive a bloock type, (which determined the block color, and then a count, of that many locations.
-		// we do that, for each block type present in the image.
-
-
-
-
-
-				// else printf("error: unknown command. "
-				// "can either be:\n\t"
-				// "halt\n\t"
-				// "ping\n\t"
-				// "display\n\t"
-				// "chat\n\t"
-				// "quit\n\t"
-				// "\n");
-
-
-
-
-
-
-// fgets(buffer, sizeof buffer, stdin);
-
-
-
-
-
-// char buffer[1024] = {0};
-
-    // while (1) {
-        // printf("UDP CLIENT[%s:%d]:> ", ip, port);
-        // fgets(buffer, sizeof buffer, stdin);
-        // if (!strcmp(buffer, "quit\n")) break;
-
-
-
-
-
-
-				// if (key[SDL_SCANCODE_G]) {
-				// 	SDL_Log("G : send display packet\n"); 
-				// 	command = display;
-				// 	write(connection, &command, 1);
-
-				// 	n = read(connection, &screen_block_count, 4);
-				// 	if (n == 0) { disconnected(); }
-				// 	else if (n < 0) { read_error(); }
-
-				// 	n = read(connection, screen, screen_block_count * 2);
-				// 	if (n == 0) { disconnected(); }
-				// 	else if (n < 0) { read_error(); }
-				// }
-
-
-
-
-        // memset(buffer, 0, sizeof buffer);
-
-
-
-
-
-        // if (n == 0) {
-        //     printf("UDP client:read disconnected.\n");
-        //     printf("{UDP SERVER DISCONNECTED}\n");
-        //     break;
-        // }
-        // printf("UDP server says: %s\n", buffer);
-    // }
-    
-// }
-//
-
-
-
-
-
-// int udp_connection = socket(AF_INET, SOCK_DGRAM, 0);
-	// if (udp_connection < 0) { perror("socket"); exit(1); }
-
-	// struct sockaddr_in udp_servaddr = {0};
-	// udp_servaddr.sin_addr.s_addr = inet_addr(ip);
-	// udp_servaddr.sin_port = htons(port + 1);
-	// udp_servaddr.sin_family = AF_INET;
-	// socklen_t len = sizeof(udp_servaddr);
-
-	// printf("%s is connecting to UDP server %s : %d...\n", player_name, ip, port + 1);
-        
-	// printf("note: about to send ackUDP!!...\n");
-	// usleep(10000);
-
-	// printf("sending ACK to server for UDP con\n");
-
-	// while (1) {
-		
-	// 	if (n == 0) printf("sendto n = 0\n");
-	// 	else if (n < 0) printf("sendto n < 0\n");
-		
-	// 	response = 0;
-	// 	// printf("RECVing...\n");
-	// 	n = recvfrom(udp_connection, &response, 1, MSG_DONTWAIT, (struct sockaddr*) &udp_servaddr, &len);
-	// 	if (n == 0 or n < 0 or response != 1) {
-	// 		printf("error: failed to receive: n = %zd. trying again....\n", n);
-	// 		continue;
-	// 	} else break;
-	// }
-
-	// u8 ack = 1;
-		// printf("SENDing...\n");
-
-
-	// while (1) {
-	// 	n = sendto(udp_connection, &response, 1, 0, (struct sockaddr*) &udp_servaddr, len);
-	// 	if (n > 0) break;
-	// }
-
-	// while (1) {
-	// 	n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*) &udp_servaddr, &len);
-	// 	if (n > 0) break;
-	// }
-
-	// usleep(100);
-	// n = sendto(udp_connection, &response, 1, 0, (struct sockaddr*) &udp_servaddr, len);
-	// usleep(100);
-	// printf("DONE sending ACK to server for UDP con\n");
-
-
-	// sendto(udp_connection, &ack, 1, 0, (struct sockaddr*)&server_addr, server_struct_length)
-
-
-
-
-
-
-
-// n = sendto(udp_connection, &response, 1, 0, (struct sockaddr*) &udp_servaddr, len);
-		
-		// printf("receiving block count first...\n");
-		// n = recvfrom(udp_connection, &screen_block_count, 4, 0, (struct sockaddr*) &udp_servaddr, &len);
-		// check(n);
-
-		// printf("sending ACK for bc...\n");
-		// sendto(udp_connection, &ack, 1, 0, (struct sockaddr*) &udp_servaddr, len);
-		
-		// printf("receiving %d blocks...\n", screen_block_count);
-		// n = recvfrom(udp_connection, screen, screen_block_count * 2, 0, (struct sockaddr*) &udp_servaddr, &len);
-		// check(n);
-
-		// printf("sending ACK for block array...\n");
-		// response = 1;
-		// sendto(udp_connection, &response, 1, 0, (struct sockaddr*) &udp_servaddr, len);
-
-
-
-
-
-// // SDL_Window* window, 
-	// int w = window_width + , h = window_height + 1;
-	// //if (h > 10000 or w > 10000) return;
-	// SDL_RenderSetLogicalSize(renderer, w, h);
-
-
-
-// if (h < 2 or w < 2) return;
-	// if (h < 2 or w < 2) return;
-// window_width = w;
-	// window_height = h;
-	// printf("width and height: (%d, %d)\n", window_width, window_height);
 
 
 
--- server.c
+++ server.c
@@ -36,10 +36,8 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
-
 static const u32 max_player_count = 32;
 static const u32 attempt_count = 10000;
-
 
 enum commands {	
 	null_command = 0,
@@ -80,7 +78,6 @@ static u8* universe = NULL;
 static u32 player_count = 0;
 static struct player* players = NULL;
 
-
 #define read_error() \
 	{do { \
 		printf("debug: server: read error! (n < 0) file:%s line:%d func:%s\n", __FILE__, __LINE__, __func__); \
@@ -100,28 +97,15 @@ static struct player* players = NULL;
 		abort(); \
 	} while(0);} \
 
-
 #define check(n) \
 	if (n == 0) { disconnected(); } \
 	else if (n < 0) { read_error(); } \
-
-
-// static inline void show() {
-// 	printf("\nDEBUG: state:  s = %llu, count = %llu\n\n", s, count);
-// 	printf("{ \n");
-// 	for (u64 i = 0; i < count; i++) {
-// 		if (i and (i % 8) == 0) puts("");
-// 		printf("%02hhx ", universe[i]);
-// 	}
-// 	printf("}\n");
-// }
 
 static inline u8 at(u64 x, u64 y, i64 x_off, i64 y_off) {
 	u64 yo = (u64)((i64)y + y_off + (i64)s) % s;
 	u64 xo = (u64)((i64)x + x_off + (i64)s) % s;
 	return universe[xo * s + yo];
 }
-
 
 static inline u64 square_root(u64 op) {
     u64 res = 0, one = 0x4000000000000000; 
@@ -164,7 +148,6 @@ static inline void load_state(const char* source) {
 	universe = malloc(count);	
 	fread(universe, 1, count, file);
 	fclose(file);
-
 	printf("loaded %llu bytes of existing universe (s=%llu)...\n", count, s);
 }
 
@@ -182,9 +165,7 @@ static inline void generate() {
 	universe = malloc(count);
 	printf("debug: generating universe of %llu bytes ...\n", count);
 
-	for (u64 i = 0; i < count; i++) universe[i] = 0;
-	// universe[2] = 5;
-
+	// for (u64 i = 0; i < count; i++) universe[i] = 0;
 	for (u64 i = 0; i < count; i++) universe[i] = (rand() % 2) * (rand() % 2) * (rand() % 2);
 }
 
@@ -276,7 +257,6 @@ static void* client_handler(void* raw) {
 		n = read(client, &command, 1);
 		if (n == 0) { printf("{CLIENT DISCONNECTED}\n"); break; } 
 		check(n);
-	
 		// printf("[received command --> %d]\n", command);
 
 		if (command == halt) {
@@ -337,11 +317,8 @@ static void* client_handler(void* raw) {
 			}
 
 		screen_full:
-
 			// pad zeros for last packet:
-
 			for (u32 i = screen_block_count; i % 128; i++) screen[i] = 0;
-
 			// printf("sending %d coords,(2-per-block)...\n", screen_block_count);
 			write(client, &screen_block_count, sizeof(u32));	
 
@@ -444,374 +421,6 @@ int main(const int argc, const char** argv) {
 
 
 // ---------------------------------------------------- dead code ------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// for (u64 i = 0, a = 0; i < 100; a = i * i, i++) 
-	// 	printf("\rsqrt(%llu) = %llu\n", a, square_root(a));
-
-	// exit(1);
-
-
-
-
-                        
-        //wprintf("SERVER:> ");
-        //fgets(buffer, sizeof buffer, stdin);
-        //n = write(client, buffer, sizeof buffer);
-        //if ( n == 0) {
-        //    printf("server:write disconnected.\n");
-        //    break;
-        //}
-        //if (!strcmp(buffer, "quit\n")) break;    
-
-
-
-
-
-
-/*
- void* handler(void* raw) {
-     struct client parameters = *(struct client*)raw;
-     int client = parameters.connection;
-     const char* ip = parameters.ip;
-     
-     printf("connected to %s\n", ip);
-     
-     char buffer[1000] = {0};
-     ssize_t n = 0;
-     while (server_running) {
-         
-         memset(buffer, 0, sizeof buffer);
-         n = read(client, buffer, sizeof buffer);
-         if ( n == 0) {
-             printf("server:read disconnected.\n");
-             printf("{CLIENT DISCONNECTED}\n");
-             break;
-         }
-         printf("client says: %s\n", buffer);
-         
-         if (!strcmp(buffer, "halt\n")) {
-             printf("halting server...\n");
-             halt_server(); continue;
-         }
-         
-         printf("SERVER:> ");
-         fgets(buffer, sizeof buffer, stdin);
-         n = write(client, buffer, sizeof buffer);
-         if ( n == 0) {
-             printf("server:write disconnected.\n");
-             break;
-         }
-         if (!strcmp(buffer, "quit\n")) break;
-     }
-     close(client);
-     free(raw);
-     return NULL;
- }
- 
- */
-
-
-
-
-
-
-/*static void connect_to_udp_client(unsigned int port) {
-
-    int connection = socket(AF_INET, SOCK_DGRAM, 0);
-    if (!connection) { perror("socket"); return; }
-    
-    struct sockaddr_in servaddr, cliaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(port);
-    servaddr.sin_family = AF_INET;
-    socklen_t len = sizeof(cliaddr);
-    bind(connection, (struct sockaddr*) &servaddr, sizeof(servaddr));
-    
-    printf("listening for UDP clients...\n");
-    
-    char buffer[1000] = {0};
-        
-    while (server_running) {
-
-        memset(buffer, 0, sizeof buffer);
-        ssize_t n = recvfrom(connection, buffer, sizeof buffer, 0, (struct sockaddr*)&cliaddr, &len);
-        if (n == 0) {
-            printf("{UDP CLIENT DISCONNECTED}\n");
-            break;
-        }
-        printf("UDP client says: %s\n", buffer);
-        
-        if (!strcmp(buffer, "halt\n")) {
-            printf("halting UDP server...\n");
-            halt_server(); continue;
-        }
-
-        printf("UDPSERVER:> ");
-        fgets(buffer, sizeof buffer, stdin);
-        if (!strcmp(buffer, "quit\n")) break;
-        sendto(connection, buffer, strlen(buffer), 0, (struct sockaddr*)&cliaddr, len);
-    }
-    close(connection);
-}
-*/
-
-
-
-
-
-
-
-
-// connect_to_udp_client(port); // ?
-
-
-
-
-
-// static inline noreturn void not_acked() {
-// 	printf("debug: error: command not acknowledged from client\n");
-// 	abort();
-// }
-
-
-
-
-		// } else if (command == ping) {
-		// 	printf("SERVER WAS PINGED!!!\n");
-		// 	write(client, &ack, 1);
-
-		// } else if (command == chat) {
-
-		// 	memset(buffer, 0, sizeof buffer);
-		// 	n = read(client, buffer, sizeof buffer);
-		// 	if (n == 0) { printf("{CLIENT DISCONNECTED}\n"); break; } 
-		// 	else if (n < 0) { read_error(); break; }
-		// 	printf("client said chat message: %s\n", buffer);
-		// 	strcat(transcript, buffer);
-		// 	write(client, &ack, 1);
-
-
-
-
-
-
-
-
-
-// n = read(client, &response, 1);
-			// if (n == 0) { printf("{CLIENT DISCONNECTED}\n"); break; } 
-			// else if (n < 0) { read_error(); break; }
-			// if (response != 1) not_acked();
-
-
-
-
-	// while (server_running) {
-
-//		ssize_t n = recvfrom(udp_connection, buffer, sizeof buffer, 0, (struct sockaddr*)&cliaddr, &len);
-		// if (n == 0) { disconnected(); }
-
-		// printf("UDP client says: %s\n", buffer);
-
-		// if (!strcmp(buffer, "halt\n")) {
-		// 	printf("halting UDP server...\n");
-		// 	halt_server(); continue;
-		// }
-
-		// printf("UDPSERVER:> ");
-		// fgets(buffer, sizeof buffer, stdin);
-		// if (!strcmp(buffer, "quit\n")) break;
-
-//		sendto(udp_connection, buffer, strlen(buffer), 0, (struct sockaddr*)&cliaddr, len);
-	// }
-
-
-
-
-
-
-// printf("starting up display thread...\n");
-	// pthread_t display_handler_thread;
-	// pthread_create(&display_handler_thread, NULL, display_client_handler, &parameters);
-
-
-
-
-
-// n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len); 
-		
-// 		printf("debug: sending DP with %d blocks...\n", screen_block_count);
-
-// 		printf("sending block count...\n");
-// 		sendto(udp_connection, &screen_block_count, 4, 0, (struct sockaddr*)&cliaddr, len);
-
-// 		// printf("waiting for udp dp ack!\n");
-// 		// n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len);
-// 		// // check(n); 
-// 		// // if (response != 1) not_acked();
-
-// 		printf("sending blocks...\n");
-// 		sendto(udp_connection, screen, screen_block_count * 2, 0, (struct sockaddr*)&cliaddr, len);
-
-// 		printf("waiting for udp dp ack!\n");
-// 		n = recvfrom(udp_connection, &response, 1, /*MSG_DONTWAIT*/0, (struct sockaddr*)&cliaddr, &len);
-// 		check(n); 
-// 		// if (response != 1) not_acked();
-
-
-
-
-
-
-
-// static void* display_client_handler(void* raw) {
-
-// 	ssize_t n = 0;
-// 	u8 response = 0;
-
-// 	struct client parameters = *(struct client*)raw;
-
-// 	struct player* player = players + parameters.player;
-// 	const char* ip = parameters.ip;
-// 	u16 port = parameters.port;
-
-// 	printf("arrived in DISPLAY HANDLER: connected to  %s : %d,   player # %d.\n", ip, port + 1, parameters.player);
-	
-// 	u32 screen_block_count = 0;
-// 	u16* screen = malloc(max_block_count * 2);
-
-// 	printf("creating display socket...\n");
-// 	int udp_connection = socket(AF_INET, SOCK_DGRAM, 0);
-// 	if (!udp_connection) { perror("socket"); abort(); }
-
-// 	struct sockaddr_in servaddr, cliaddr;
-// 	memset(&servaddr, 0, sizeof(servaddr));
-// 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-// 	servaddr.sin_port = htons(port + 1);
-// 	servaddr.sin_family = AF_INET;
-// 	socklen_t len = sizeof(cliaddr);
-// 	bind(udp_connection, (struct sockaddr*) &servaddr, sizeof(servaddr));
-// 	printf("debug: display client handler: setup udp server on port %d\n", port + 1);
-
-// 	usleep(1000000);
-	
-// 	printf("debug: display client handler: waiting for client to sendto ACK first...\n");
-	
-// 	// bool should_continue = 1;
-// 	// response = 0;
-
-// 	// while (1) {
-// 	// 	n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len); // MSG_DONTWAIT / MSG_WAITALL   MSG_DONTWAIT
-// 	// 	if (n > 0) break;
-// 	// }
-
-// 	// while (1) {
-// 	// 	n = sendto(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, len);
-// 	// 	if (n > 0) break;
-// 	// }
-
-// 	printf("debug: in recvfrom, waiting for client to ACK...\n");
-// 	usleep(1000);
-// 	n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len); 
-
-// 	// if (n > 0) break;
-
-// 	//0 or response != 1) {
-// 		// 	printf("error: failed to receive: n = %zd. trying again....\n", n);
-// 		// 	should_continue = 0; 
-// 		// } else should_continue = 1;
-// 		// printf("SENDing...\n");
-// 		// u8 ack = 1;
-// 		// sendto(udp_connection, &ack, 1, 0, (struct sockaddr*)&cliaddr, len);
-// 		// if (n == 0) printf("sendto n = 0\n");
-// 		// else if (n < 0) printf("sendto n < 0\n");
-// 		// if (should_continue) continue; else break;
-// 		// usleep(1000);
-
-
-// 	printf("debug: display client handler: DONE! received.\n");
-
-// 	while (player->active) {
-		
-// 		screen_block_count = (rand() % 10000) * 2 + 2;
-
-// 		for (u32 i = 0; i < screen_block_count; i += 2) {
-// 			screen[i] = rand() % player->width;
-// 			screen[i + 1] = rand() % player->height;
-// 		}
-
-// 		// n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len); 
-		
-// 		printf("debug: sending DP with %d blocks...\n", screen_block_count);
-
-// 		printf("sending block count...\n");
-// 		sendto(udp_connection, &screen_block_count, 4, 0, (struct sockaddr*)&cliaddr, len);
-
-// 		// printf("waiting for udp dp ack!\n");
-// 		// n = recvfrom(udp_connection, &response, 1, 0, (struct sockaddr*)&cliaddr, &len);
-// 		// // check(n); 
-// 		// // if (response != 1) not_acked();
-
-// 		printf("sending blocks...\n");
-// 		sendto(udp_connection, screen, screen_block_count * 2, 0, (struct sockaddr*)&cliaddr, len);
-
-// 		printf("waiting for udp dp ack!\n");
-// 		n = recvfrom(udp_connection, &response, 1, /*MSG_DONTWAIT*/0, (struct sockaddr*)&cliaddr, &len);
-// 		check(n); 
-// 		// if (response != 1) not_acked();
-
-// 		printf("GOT ACK! done with dp.!\n");
-
-// 		usleep(100000);
-// 	}
-// 	printf("debug: closing display connection...\n");
-// 	close(udp_connection);
-// 	free(screen);
-// 	return 0;
-// }
-
-
 
 
 
