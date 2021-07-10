488399ea54bc7c540413260d4bfa1a896e7e583f
dwrrehman <dwrrehman@gmail.com>
2021-07-09 20:26:34 -0700

    push the new server code, and slightly tweaked client code.

--- source/client.c
+++ source/client.c
@@ -8,7 +8,7 @@
 
 #include <SDL2/SDL.h>
 
-#define check(n) { if (n == 0 || n < 0) printf("error(%ld): %s line:%d func:%s\n", n, __FILE__, __LINE__, __func__); }
+#define check(n) do { if (n == 0 || n < 0) printf("error(%ld): %s line:%d func:%s\n", n, __FILE__, __LINE__, __func__); } while (0)
 
 int main(const int argc, const char** argv) {
 	if (argc != 3) exit(puts("usage: ./client <ip> <port>"));
@@ -38,19 +38,21 @@ int main(const int argc, const char** argv) {
 	if (SDL_Init(SDL_INIT_VIDEO)) exit(printf("SDL_Init failed: %s\n", SDL_GetError()));
 	SDL_Window *window = SDL_CreateWindow("universe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 400, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 20, 15);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 22, 17);
 	SDL_ShowCursor(0);
 	
-	uint32_t* buffer = malloc(20 * 15 * 4);
+	const int buffer_count = 22 * 17 * 4;
+
+	uint32_t* buffer = malloc(buffer_count);
 	uint32_t* pixels = NULL;
 	int pitch = 0;
 	bool quit = false, fullscreen = false;
 
 	while (not quit) {
 		uint32_t start = SDL_GetTicks();
-		recvfrom(fd, buffer, 20 * 15 * 4, MSG_DONTWAIT, (struct sockaddr*) &address, &size); 
+		recvfrom(fd, buffer, buffer_count, MSG_DONTWAIT, (struct sockaddr*) &address, &size); 
 		SDL_LockTexture(texture, NULL, (void**) &pixels, &pitch);		
-		memcpy(pixels, buffer, 20 * 15 * 4);
+		memcpy(pixels, buffer, buffer_count);
 		SDL_UnlockTexture(texture);
 		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
@@ -60,37 +62,49 @@ int main(const int argc, const char** argv) {
 			const Uint8* key = SDL_GetKeyboardState(0);
 			if (event.type == SDL_QUIT) quit = true;
 			if (event.type == SDL_KEYDOWN) {
-				if (key[SDL_SCANCODE_GRAVE]) SDL_SetWindowFullscreen(window, (fullscreen = !fullscreen) ? SDL_WINDOW_FULLSCREEN : 0);
-				if (key[SDL_SCANCODE_0] or key[SDL_SCANCODE_ESCAPE] or key[SDL_SCANCODE_Q]) quit = true;
-				if (key[SDL_SCANCODE_0]) sendto(fd, "H", 1, 0, (struct sockaddr*) &address, size);
 
-				if (key[SDL_SCANCODE_W]) sendto(fd, "w", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_TAB]) SDL_SetWindowFullscreen(window,(fullscreen=!fullscreen)?SDL_WINDOW_FULLSCREEN:0);
+				if (key[SDL_SCANCODE_ESCAPE] || key[SDL_SCANCODE_GRAVE]) quit = true;
+				if (key[SDL_SCANCODE_GRAVE]) sendto(fd, "H", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_RETURN]) sendto(fd, "S", 1, 0, (struct sockaddr*) &address, size);
+
 				if (key[SDL_SCANCODE_A]) sendto(fd, "a", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_S]) sendto(fd, "s", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_B]) sendto(fd, "b", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_C]) sendto(fd, "c", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_D]) sendto(fd, "d", 1, 0, (struct sockaddr*) &address, size);
-
-				if (key[SDL_SCANCODE_T]) sendto(fd, "t", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_E]) sendto(fd, "e", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_F]) sendto(fd, "f", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_G]) sendto(fd, "g", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_H]) sendto(fd, "h", 1, 0, (struct sockaddr*) &address, size);
-
 				if (key[SDL_SCANCODE_I]) sendto(fd, "i", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_J]) sendto(fd, "j", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_K]) sendto(fd, "k", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_L]) sendto(fd, "l", 1, 0, (struct sockaddr*) &address, size);
-
-				if (key[SDL_SCANCODE_R]) sendto(fd, "r", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_E]) sendto(fd, "e", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_X]) sendto(fd, "x", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_C]) sendto(fd, "c", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_V]) sendto(fd, "v", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_Z]) sendto(fd, "z", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_U]) sendto(fd, "u", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_Y]) sendto(fd, "y", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_N]) sendto(fd, "n", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_M]) sendto(fd, "m", 1, 0, (struct sockaddr*) &address, size);
-				if (key[SDL_SCANCODE_P]) sendto(fd, "p", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_N]) sendto(fd, "n", 1, 0, (struct sockaddr*) &address, size);
 				if (key[SDL_SCANCODE_O]) sendto(fd, "o", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_P]) sendto(fd, "p", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_Q]) sendto(fd, "q", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_R]) sendto(fd, "r", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_S]) sendto(fd, "s", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_T]) sendto(fd, "t", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_U]) sendto(fd, "u", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_V]) sendto(fd, "v", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_W]) sendto(fd, "w", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_X]) sendto(fd, "x", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_Y]) sendto(fd, "y", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_Z]) sendto(fd, "z", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_0]) sendto(fd, "0", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_1]) sendto(fd, "1", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_2]) sendto(fd, "2", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_3]) sendto(fd, "3", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_4]) sendto(fd, "4", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_5]) sendto(fd, "5", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_6]) sendto(fd, "6", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_7]) sendto(fd, "7", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_8]) sendto(fd, "8", 1, 0, (struct sockaddr*) &address, size);
+				if (key[SDL_SCANCODE_9]) sendto(fd, "9", 1, 0, (struct sockaddr*) &address, size);
+				
 			}
 		}
 
@@ -113,184 +127,5 @@ int main(const int argc, const char** argv) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// todo     add client side scale back in, to ajust the logical size of the window, both x and y?  idk... 
-// x & y are bot independantly changable with the server side width view and height of view. (which should match up with the clients logical height and view?.... hmmm thats interesting..
-// we can make it so that they are always insync? maybe? except for packet loss... :( owell...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-		the idea is to send a resize command byte, and then a 5 byte packet of    { continutation_byte, w=u16, h=u16 }.
-
-			thats it!
-		just make sure that when we receive the 5 byte packet, that it says a continutation byte,    to verify its correct. 
-
-		also make sure that the command receiver ignores the continutation byte packets. 
-
-			thaats it!
-
-
-
-
-
-s
-*/
-
-
-
-
-
-
-
-
-
-// ------------------------------------------------ work in progress:---------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// static inline void zoom_in(SDL_Renderer* renderer) {
-// 	int future_scaled_width = (int) ((float)window_width * (scale - 0.005f));
-// 	int future_scaled_height = (int) ((float)window_height * (scale - 0.005f));
-// 	if (not future_scaled_width or not future_scaled_height) return;
-// 	scale -= 0.001f;
-// 	rescale(renderer);
-// }
-
-// static inline void zoom_out(SDL_Renderer* renderer) {
-// 	if (scale >= 0.99f) return;
-// 	scale += 0.001f;
-// 	rescale(renderer);
-// }
-
-
-
-// static inline void send_resize_command(int connection) {
-// 	u8 command = view_resized, response = 0;
-// 	write(connection, &command, 1);
-// 	write(connection, &scaled_width, 2);
-// 	write(connection, &scaled_height, 2);
-// 	ssize_t n = read(connection, &response, 1); 
-// 	check(n); if (response != 1) not_acked();
-// }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-// static void interrupt_kill(int __attribute__((unused)) _) {
-// 	kill(getpid(), SIGINT);
-// }	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-  //   		SDL_RenderClear(renderer);
-
-		// // SDL_SetRenderDrawColor(renderer, 67, 45, 234, 255);
-		// // SDL_RenderDrawPoint(renderer, width_radius, height_radius);
-
-		// SDL_SetRenderDrawColor(renderer, colors[3 + 0], colors[3 + 1], colors[3 + 2], 255);
-		// for (u32 i = 0; i < screen_block_count; i += 2) {
-		// 	SDL_RenderDrawPoint(renderer, screen[i], screen[i + 1]);
-		// }
-
-
-
-
-
- // , SDL_Renderer* renderer, SDL_Texture** texture
-// SDL_DestroyTexture(*texture);
-	// *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
-
-
-// static inline void toggle_fullscreen(SDL_Window* window) { // , SDL_Renderer* renderer, SDL_Texture** texture
-	
-// }
 
 
